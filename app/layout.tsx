5578ef3c69b5328f9c97f69ef9288a01a79e0d94
bazelisek <windynour@gmail.com>
2025-10-16 16:18:53 +0200

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 import ReduxWrapper from "@/store/ReduxWrapper";
 import Header from "@/components/Header";
@@ -32,6 +33,7 @@ export default function RootLayout({
         <ReduxWrapper>
           {children}
         </ReduxWrapper>
+        <Analytics />
       </body>
     </html>
   );
